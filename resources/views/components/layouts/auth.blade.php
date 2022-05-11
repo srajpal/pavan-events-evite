925fad11e78d4039d8722cfb8a187301554edca4
SRajpal <srajpal@gmail.com>
2022-05-11 09:49:00 -0400

    updates

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,6 +1,8 @@
 <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <x-main.navbar />
-    {{ $slot }}
+    <div class="container-fluid py-4 flex-grow-1">
+        {{ $slot }}
+    </div>
     <x-main.footer />
 </main>
 <x-main.settings />
